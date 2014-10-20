63d25e0086d7012afddc25440c3cef722ab58f1f
Johannes Grassler <j.grassler@syseleven.de>
2014-10-20 09:51:55 +0200

    Fixed ipaddress_{eth0,vhost0} resource export for dev clouds.

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,10 +3,20 @@
 # export/import hosts and ips
 #
 class sys11hosts() {
-  @@host { $::fqdn:
-    ip           => $::ipaddress_eth0, 
-    host_aliases => $::hostname,
+  if $::ipaddress_eth0 {
+    @@host { $::fqdn:
+      ip           => $::ipaddress_eth0, 
+      host_aliases => $::hostname,
+    }
   }
+
+  if $::ipaddress_vhost0 {
+    @@host { $::fqdn:
+      ip           => $::ipaddress_vhost0, 
+      host_aliases => $::hostname,
+    }
+  }
+
 
   Host <<| |>>
 }
