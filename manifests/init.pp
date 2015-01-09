21ca596d3a667c34035929a666a9ad119b406522
Stefan Andres <s.andres@syseleven.de>
2015-01-09 10:40:16 +0100

    os-194 support ipaddress for host-entry from any interface

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,18 +2,31 @@
 #
 # export/import hosts and ips
 #
-class sys11hosts() {
-  if $::ipaddress_eth0 {
-    @@host { $::fqdn:
-      ip           => $::ipaddress_eth0, 
-      host_aliases => $::hostname,
+class sys11hosts(
+  $interface = false,
+) {
+  if $interface {
+    $ipaddress_fact = "::ipaddress_$interface"
+    $ipaddress = inline_template('<%= scope.lookupvar(ipaddress_fact) %>')
+    if $ipaddress {
+      @@host { $::fqdn:
+        ip           => $::ipaddress,
+        host_aliases => $::hostname,
+      }
     }
-  }
+  } else {
+    if $::ipaddress_eth0 {
+      @@host { $::fqdn:
+        ip           => $::ipaddress_eth0,
+        host_aliases => $::hostname,
+      }
+    }
 
-  if $::ipaddress_vhost0 {
-    @@host { $::fqdn:
-      ip           => $::ipaddress_vhost0, 
-      host_aliases => $::hostname,
+    if $::ipaddress_vhost0 {
+      @@host { $::fqdn:
+        ip           => $::ipaddress_vhost0,
+        host_aliases => $::hostname,
+      }
     }
   }
 
